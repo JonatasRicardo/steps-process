ef8dd6fa1841f9ed4aac2430ae83a2414c268bd7
Jonatas Santos <jonatasricardo90@gmail.com>
2020-04-24 15:25:17 -0300

    update modal storybook

--- src/components/Modal/__stories__/Modal.stories.js
+++ src/components/Modal/__stories__/Modal.stories.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import Modal from '../Modal';
+import ModalHeader from '../ModalHeader';
 
 export default { title: 'Modal' };
 
-export const ModalActive = () => <Modal ative />;
+export const ModalActive = () => <Modal active />;
+export const ModalActiveWithHeader = () => (
+    <Modal active>
+        <ModalHeader
+            title="Engagement Process"
+            subtitle="2019 Fall Oncology Advisory Board"
+            label="Abandoned"
+            hasCloseButton
+        />
+    </Modal>
+);
