c76e77359b78583b64e63137ce08dc4f0f1637e0
Jonatas Santos <jonatasricardo90@gmail.com>
2020-04-24 15:25:29 -0300

    Button Storybook

---
+++ src/components/Button/__stories__/Button.stories.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from '../';
+
+export default { title: 'Button' };
+
+export const ButtonText = () => <Button>Text</Button>;
+export const ButtonLight = () => <Button light>Light</Button>;
