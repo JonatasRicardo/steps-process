7da577c039c98d69492aee11b581d7caf020abf3
Jonatas Santos <jonatasricardo90@gmail.com>
2020-04-24 01:33:51 -0300

    Steps snapshot testing

--- src/components/Steps/__test__/Steps.test.js
+++ src/components/Steps/__test__/Steps.test.js
@@ -3,6 +3,7 @@ import { render } from '@testing-library/react';
 import StepItem from '../StepItem';
 import StepsContainer from '../StepsContainer';
 
+describe('<StepsContainer><StepItem/></StepsContainer>', () => {
   it('renders Steps components', () => {
     const { getByTestId, getAllByTestId } = render(
       <StepsContainer>
@@ -15,6 +16,21 @@ import StepsContainer from '../StepsContainer';
     const stepItemElements = getAllByTestId('step-item');
     expect(stepsContainerElement).toBeDefined();
     expect(stepItemElements).toHaveLength(3)
+  });
+
+  it('Snapshot', () => {
+    const { getByTestId } = render(
+      <StepsContainer currentStep={6}>
+        <StepItem title="Draft" statusName="Created on" statusDate="01/31/2020" />
+        <StepItem title="Pending Approval" statusName="Approval sent" statusDate="02/14/2020" />
+        <StepItem title="Approved" statusName="Abandoned on" statusDate="02/20/2020" />
+        <StepItem title="Execute" />
+        <StepItem title="Concluded" />
+        <StepItem title="Closed" />
+    </StepsContainer>
+    );
+    const stepsContainerElement = getByTestId('steps-container');
+    expect(stepsContainerElement).toMatchSnapshot();
   });
 
   it('Current Step classes', () => {
@@ -32,3 +48,4 @@ import StepsContainer from '../StepsContainer';
     expect(stepsContainerElement.children[2]).not.toHaveClass('is-done');
     expect(stepsContainerElement.children[2]).not.toHaveClass('is-next-done');
   });
+})
